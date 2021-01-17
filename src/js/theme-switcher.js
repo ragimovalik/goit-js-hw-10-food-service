469b466f3d7110a5aee2bf14a89c0cb29247766b
ragimovalik <ragimovalik@gmail.com>
2021-01-17 15:16:54 +0200

    Structured and cleaned js folders&files

---
+++ src/js/theme-switcher.js
@@ -0,0 +1,31 @@
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const bodyEl = document.querySelector('body');
+const themeSwitchEl = document.querySelector('#theme-switch-toggle');
+
+themeSwitchEl.addEventListener('click', themeSwitcher);
+if (Object.values(localStorage).includes('dark-theme')) {
+  darkThemeAdder();
+}
+
+function themeSwitcher() {
+  bodyEl.classList.contains('dark-theme')
+    ? lightThemeAdder()
+    : darkThemeAdder();
+}
+
+function lightThemeAdder() {
+  bodyEl.classList.remove('dark-theme') || bodyEl.classList.add('light-theme');
+  localStorage.removeItem(Theme.DARK) ||
+    localStorage.setItem('theme', Theme.LIGHT);
+}
+
+function darkThemeAdder() {
+  bodyEl.classList.add('dark-theme') || bodyEl.classList.remove('light-theme');
+  localStorage.removeItem(Theme.LIGHT) ||
+    localStorage.setItem('theme', Theme.DARK);
+  themeSwitchEl.checked = true;
+}
